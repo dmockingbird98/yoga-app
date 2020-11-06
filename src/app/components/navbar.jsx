1d341fc80ed6807b210510889980b849da37486e
dmockingbird98 <anand.raghu98@gmail.com>
2020-11-06 18:07:04 +0530

    navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {Nav} from "react-bootstrap";
 
-
-function NavBar(){
+export default function NavBar(){
   let key = "requests";
 
   if(window.location.href.search("service") >=0 ) key = "services"
   else if(window.location.href.search("payment") >=0) key = "payments"
   else key = "requests";
-  
+
     return (
         <div className="subHeader d-flex align-items-center">
         <i className="fas fa-chevron-left"></i>
@@ -26,4 +25,3 @@ function NavBar(){
       </div>
     )
 }
-export default NavBar
